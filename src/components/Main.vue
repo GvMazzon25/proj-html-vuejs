1cbdc98a705f22d0953a319ff35dc3e6858a83b5
GvMazzon25 <gvmazzon25@gmail.com>
2021-12-11 19:58:28 +0100

    Section2

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="contain">
     <Section1 />
-    
+    <Section2 />
   </div>
 </template>
 
 <script>
 import Section1 from '@/components/Section1.vue'
+import Section2 from '@/components/Section2.vue'
 
 export default {
   name:'Main',
   components:{
-    Section1
+    Section1,
+    Section2,
   }
 }
 </script>
---
+++ src/components/Section2.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="contain">
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='scss'>
+
+</style>
